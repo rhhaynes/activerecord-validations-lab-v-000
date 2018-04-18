526b3be129f7bc5db63c3523d42a104c545cd69c
Randall Harris Haynes <harrishaynes@gmail.com>
2018-04-18 19:00:00 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, :presence => true
-  validate :title_is
+  validate  :title_is_clickbait
   validates :content, :length {:minimum => 250}
   validates :summary, :length {:maximum => 250}
   validates :category, :inclusion => {:in => ['Fiction', 'Non-Fiction']}
