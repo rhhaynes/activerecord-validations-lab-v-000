921de85e20b32bea7e2e870746d346a21f046a0a
Randall Harris Haynes <harrishaynes@gmail.com>
2018-04-18 19:23:02 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, :presence => true
+  validate  :title_is_clickbait
+  validates :content, :length => {:minimum => 250}
+  validates :summary, :length => {:maximum => 250}
+  validates :category, :inclusion => {:in => ['Fiction', 'Non-Fiction']}
+  
+  def title_is_clickbait
+    clickbait = [/Won't Believe/, /Secret/, /Top/, /Guess/]
+    if !clickbait.any?{|regexp| !!regexp.match(title)}
+      errors.add(:title, "must be sufficiently clickbait-y")
+    end
+  end
 end
